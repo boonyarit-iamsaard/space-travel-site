6cfd3e0498a87265ef89093e3b2b572b6e665ae2
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-06-22 21:06:35 +0700

    implement destination tab functionality

--- navigation.js
+++ navigation.js
@@ -1,10 +1,7 @@
 const nav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
-// when someone clicks on the hamburger button
 navToggle.addEventListener('click', () => {
-  // if the navigation menu is closed, open it
-  // if the navigation menu is open, close it
   const visibility = nav.getAttribute('data-visible');
 
   if (visibility === 'false') {
