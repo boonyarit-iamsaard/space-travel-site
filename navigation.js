9adced550d27154675f6e77d2ffb7cb1c8d03bb9
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-06-23 21:08:18 +0700

    implement primary navigation active state

--- navigation.js
+++ navigation.js
@@ -1,5 +1,16 @@
+const currentPath = window.location.pathname;
 const nav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+
+Array.from(nav.children).forEach(item => {
+  const pathname = item.children[0].pathname;
+
+  if (pathname === currentPath) {
+    item.classList.add('active');
+  } else {
+    item.classList.remove('active');
+  }
+});
 
 navToggle.addEventListener('click', () => {
   const visibility = nav.getAttribute('data-visible');
